ce0f7336fd06d6123f61896694aa88798a808de8
Donovan Daniels <dwddaniels262@gmail.com>
2018-03-20 10:01:12 -0500

    Moved to /

--- Bot/sharder.js
+++
@@ -1,7 +0,0 @@
-const { ShardingManager } = require('discord.js');
-// dont change total shards unless your bot is in or around 2500 guilds
-const manager = new ShardingManager(`bot.js`, {totalShards: 1});
-
-manager.spawn();
-manager.on('launch', shard => {console.log(`Successfully launched shard ${shard.id}`)});
-// Shard #1 will have an ID of 0, #2 will have an ID of 1, etc..
