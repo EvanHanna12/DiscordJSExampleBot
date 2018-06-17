d69467d0526a65fa588bd83c0ed5a0e4fe3ba4a0
Donovan Daniels <dwddaniels262@gmail.com>
2018-06-16 19:45:08 -0500

    ES6 & cleanup

--- sharder.js
+++ sharder.js
@@ -1,7 +1,10 @@
 const { ShardingManager } = require('discord.js');
-// don't change total shards unless your bot is at or around 2500 guilds
+// don't change total shards unless your bot is at or around 2000 guilds (max guilds on one shard is 2500)
 const manager = new ShardingManager(`bot.js`, {totalShards: 1});
 
 manager.spawn();
-manager.on('launch', shard => {console.log(`Successfully launched shard ${shard.id}`)});
+manager.on('launch', (shard) => {
+  console.log(`Successfully launched shard ${shard.id}`)
+});
+
 // (computers count from zero) Shard #1 will have an ID of 0, #2 will have an ID of 1, etc..
